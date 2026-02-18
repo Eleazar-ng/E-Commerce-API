2f66dc802c7614edf75302e84fd1ce06ad092f34
Edward Eleazar <eleazaredward@gmail.com>
2026-02-18 11:53:43 +0100

    refactor(server): updated server setup

--- v1/src/server.ts
+++ v1/src/server.ts
@@ -1,17 +1,30 @@
+import mongoose from "mongoose";
 import { app } from "./app";
 import { env } from "./config/env";
 
 const PORT = env.PORT;
 const ENV = env.NODE_ENV;
 
-console.log(`\nStarting server...`);
-console.log(`Starting server in ${ENV} mode`);
+console.log('\x1b[35m' + `\nStarting server...`+ '\x1b[0m');
+console.log('\x1b[36m' + `Starting server in ${ENV} mode` + '\x1b[0m');
+
+// Database Connection
+mongoose.connect(env.DATABASE_URL)
+.then(() => {
+  console.log('\x1b[32m' + '\nConnected to MongoDB successfully!' + '\x1b[0m')
+})
+.catch((err) => {
+  console.log('\x1b[31m' + '\nFailed to connect to MongoDB' + '\x1b[0m')
+  console.error('\x1b[41m' + `Error connecting to MongoDB:`, err);
+  console.log('\x1b[0m')
+})
 
 app.listen(PORT, (err) => {
   if (err) {
     return console.error('something bad happened', err)
   }
 
-  console.log(`\nE-Commerce API server running on http://localhost:${PORT}`);
-  console.log(`\n!!! Version-1 root: http://localhost:${PORT}/api/v1`);
+  console.log('\x1b[34m' + `\nE-Commerce API server running on http://localhost:${PORT}`);
+  console.log(`\n!!! Version-1 root: http://localhost:${PORT}/api/v1` + '\x1b[0m');
+
 });
